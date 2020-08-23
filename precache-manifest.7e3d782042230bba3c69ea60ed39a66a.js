d6259eb4c3bc756f989791081b47a24acfec5bf2
Brandon Eng <bce25@cornell.edu>
2020-08-22 23:07:24 -0700

    Updates

---
+++ precache-manifest.7e3d782042230bba3c69ea60ed39a66a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a684751b11c0333446b163ec8a4e56a",
+    "url": "/ReactHooksExpenseList/index.html"
+  },
+  {
+    "revision": "c3a215707b6e3d9f48fb",
+    "url": "/ReactHooksExpenseList/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "03da85e9d9deaa4b606a",
+    "url": "/ReactHooksExpenseList/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c3a215707b6e3d9f48fb",
+    "url": "/ReactHooksExpenseList/static/js/2.b52dd6fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactHooksExpenseList/static/js/2.b52dd6fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03da85e9d9deaa4b606a",
+    "url": "/ReactHooksExpenseList/static/js/main.303dba3a.chunk.js"
+  },
+  {
+    "revision": "7b5e3fc49bac7a956f2a",
+    "url": "/ReactHooksExpenseList/static/js/runtime-main.311875c7.js"
+  }
+]);
